ccef03f01c940952b8e67fb06ce63941a93f5b8f
ariiesaka <judahutauruk12@gmail.com>
2023-12-02 13:54:02 +0700

    Created app search and installed ir_datasets library

--- search/views.py
+++ search/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import ir_datasets
 
-# Create your views here.
+
+def home(request):
+    dataset = ir_datasets.load('antique')
+    return HttpResponse(str(dataset.docs_iter()[0]))
